3e641d1f10ac3defe913361d73d5e7b4cf6c2ab7
gwynette <55465807+gwynette@users.noreply.github.com>
2019-09-27 22:27:05 -0400

    Add files via upload
    Added files that were modified for the assignment

---
+++ Counter.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <!--Makes sure count doesn't drop below 0-->
+    <div v-if="count<0">{{  count = 0  }}</div>
+    <div v-else>
+    <p>{{  count  }}</p>
+    </div>
+    <button @click="count++">Increase by 1</button>
+    <button @click="count--">Decrease by 1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    count:{
+
+    }
+  }
+};
+
+</script>
+
+<style>
+</style>
